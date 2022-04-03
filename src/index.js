382c6471540e80af7e099308e212989d6c746b65
Ganesh B <ganeshsurfs@gmail.com>
2022-04-03 12:28:51 +0530

    [TEMP] Minor changes to documentation of utils, proxy, process, and file function files

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ Dynamic CGI serving using dynamic path imports for
      CGI supporting executable for Interpreted languages Embedded Distribution
 Contribution: 2018 Ganesh K. Bhat <ganeshsurfs@gmail.com> 
 */
+
 /* eslint no-console: 0 */
 let fileProcess = require("./file");
 let proxy = require("./proxy");
