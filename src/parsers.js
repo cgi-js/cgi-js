5ddea19d56ab6e9c316c0381691b54a27e7ce0c7
Krishnamurthy G B <3424344+ganeshkbhat@users.noreply.github.com>
2023-07-05 02:48:24 +0000

    [COMMIT] Adding files for interfaces, parsers, and wasm modules

---
+++ src/parsers.js
@@ -0,0 +1,10 @@
+/*
+License: MIT
+Dynamic CGI serving using dynamic path imports for 
+     CGI supporting executable for Interpreted languages Embedded Distribution
+Contribution: 2018 Ganesh K. Bhat <ganeshsurfs@gmail.com> 
+*/
+
+/* eslint no-console: 0 */
+
+'use strict';
