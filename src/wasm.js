020cf8d8045b800b344e5ac2d706961a91293a38
Krishnamurthy G B <3424344+ganeshkbhat@users.noreply.github.com>
2023-07-05 07:57:32 +0530

    Update wasm.js
    Update wasm.js

---
+++ src/wasm.js
@@ -0,0 +1,21 @@
+/*
+License: MIT
+Dynamic CGI serving using dynamic path imports for 
+     CGI supporting executable for Interpreted languages Embedded Distribution
+Contribution: 2018 Ganesh K. Bhat <ganeshsurfs@gmail.com> 
+*/
+
+/* eslint no-console: 0 */
+
+'use strict';
+
+function InterfaceWasm() {
+  
+}
+
+function Wasm() {
+
+}
+
+module.eexport.InterfaceWasm = InterfaceWasm;
+module.exports.Wasm = Wasm;
