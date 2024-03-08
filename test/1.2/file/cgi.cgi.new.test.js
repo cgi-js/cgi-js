fd3d95be679852347a86a852656c544149f033cd
Ganesh B <ganeshsurfs@gmail.com>
2024-03-08 15:01:34 +0530

    [COMMIT] Adding changes to readme of the file and adding tests for 1.2.2 to be commited

--- test/1.2/file/cgi.cgi.new.test.js
+++ test/1.2/file/cgi.cgi.new.test.js
@@ -1,3 +1,46 @@
+/**
+ * 
+ * Package: 
+ * Author: Desktop-CGI, Ganesh B
+ * Email: desktopcgi@gmail.com
+ * Description: Nodejs 
+ * Install: 
+ * Github: https://github.com/
+ * npmjs Link: 
+ * File: .js
+ * File Description: 
+ * 
+*/
+
+/* eslint no-console: 0 */
+
+'use strict';
+
+const { expect } = require('chai');
+
+
+describe('[request tests] Tests to ', () => {
+  let tst, written;
+  before(() => {
+    
+  });
+
+  after(() => { });
+
+  it('should ', () => {
+    let actual = true;
+    let expected = true;
+    expect(actual).to.equal(expected);
+  });
+
+  it('should ', () => {
+    let actual = true;
+    let expected = true;
+    expect(actual).to.equal(expected);
+  });
+
+});
+
 // var request = require('supertest');
 
 // var mocha = require('mocha')
---
+++ test/1.2/file/cgi.file.cgi.processexecute.js
@@ -0,0 +1,42 @@
+/**
+ * 
+ * Package: 
+ * Author: Desktop-CGI, Ganesh B
+ * Email: desktopcgi@gmail.com
+ * Description: Nodejs 
+ * Install: 
+ * Github: https://github.com/
+ * npmjs Link: 
+ * File: .js
+ * File Description: 
+ * 
+*/
+
+/* eslint no-console: 0 */
+
+'use strict';
+
+const { expect } = require('chai');
+
+
+describe('[request tests] Tests to ', () => {
+  let tst, written;
+  before(() => {
+    
+  });
+
+  after(() => { });
+
+  it('should ', () => {
+    let actual = true;
+    let expected = true;
+    expect(actual).to.equal(expected);
+  });
+
+  it('should ', () => {
+    let actual = true;
+    let expected = true;
+    expect(actual).to.equal(expected);
+  });
+
+});
