c0ab734a963b59537beb553c19171ffc02eeb813
Ganesh K Bhat <ganeshsurfs@gmail.com>
2025-05-01 19:58:53 +0530

    [COMMIT] Adding interface implementation for every protocol possible. imports to be done

--- src/protocols.js
+++ src/protocols.js
@@ -29,76 +29,311 @@ function protocolInterface(config) {
     }
 }
 
+
+
 function tcp(config) {
     protocolInterface.call(this, config);
+    
+    this.connect = function () {
+        
+    }
+    this.disconnect = function () {
+        
+    }
+    this.serve = function () {
+        
+    }
+    this.send = function () {
+        
+    }
+    this.receive = function () {
+        
+    }
 }
 
 function tls(config) {
     protocolInterface.call(this, config);
+    
+    this.connect = function () {
+        
+    }
+    this.disconnect = function () {
+        
+    }
+    this.serve = function () {
+        
+    }
+    this.send = function () {
+        
+    }
+    this.receive = function () {
+        
+    }
 }
 
 function udp(config) {
     protocolInterface.call(this, config);
+    
+    this.connect = function () {
+        
+    }
+    this.disconnect = function () {
+        
+    }
+    this.serve = function () {
+        
+    }
+    this.send = function () {
+        
+    }
+    this.receive = function () {
+        
+    }
 }
 
 function socksv4(config) {
     protocolInterface.call(this, config);
+    
+    this.connect = function () {
+        
+    }
+    this.disconnect = function () {
+        
+    }
+    this.serve = function () {
+        
+    }
+    this.send = function () {
+        
+    }
+    this.receive = function () {
+        
+    }
 }
 
 function socksv4a(config) {
     protocolInterface.call(this, config);
+    
+    this.connect = function () {
+        
+    }
+    this.disconnect = function () {
+        
+    }
+    this.serve = function () {
+        
+    }
+    this.send = function () {
+        
+    }
+    this.receive = function () {
+        
+    }
 }
 
 function socksv5(config) {
     protocolInterface.call(this, config);
+    
+    this.connect = function () {
+        
+    }
+    this.disconnect = function () {
+        
+    }
+    this.serve = function () {
+        
+    }
+    this.send = function () {
+        
+    }
+    this.receive = function () {
+        
+    }
 }
 
 function socks(config) {
     protocolInterface.call(this, config);
+    
+    this.connect = function () {
+        
+    }
+    this.disconnect = function () {
+        
+    }
+    this.serve = function () {
+        
+    }
+    this.send = function () {
+        
+    }
+    this.receive = function () {
+        
+    }
 }
 
 function socket(config) {
     protocolInterface.call(this, config);
+    
+    this.connect = function () {
+        
+    }
+    this.disconnect = function () {
+        
+    }
+    this.serve = function () {
+        
+    }
+    this.send = function () {
+        
+    }
+    this.receive = function () {
+        
+    }
 }
 
 function ftp(config) {
     protocolInterface.call(this, config);
+    
+    this.connect = function () {
+        
+    }
+    this.disconnect = function () {
+        
+    }
+    this.serve = function () {
+        
+    }
+    this.send = function () {
+        
+    }
+    this.receive = function () {
+        
+    }
 }
 
 function ftps(config) {
     protocolInterface.call(this, config);
+    
+    this.connect = function () {
+        
+    }
+    this.disconnect = function () {
+        
+    }
+    this.serve = function () {
+        
+    }
+    this.send = function () {
+        
+    }
+    this.receive = function () {
+        
+    }
 }
 
 function ws(config) {
     protocolInterface.call(this, config);
+    
+    this.connect = function () {
+        
+    }
+    this.disconnect = function () {
+        
+    }
+    this.serve = function () {
+        
+    }
+    this.send = function () {
+        
+    }
+    this.receive = function () {
+        
+    }
 }
 
 
 function handlers(config) {
     protocolInterface.call(this, config);
 
+    let o;
+
     if (config.protocol === "udp") {
-
+        o = new udp(config);
+        this.connect = o.connect;
+        this.disconnect = o.disconnect; 
+        this.serve = o.serve;
+        this.send = o.send; 
+        this.receive = o.receive;
     } else if (config.protocol === "tcp") {
-
+        o = new tcp(config);
+        this.connect = o.connect;
+        this.disconnect = o.disconnect; 
+        this.serve = o.serve;
+        this.send = o.send; 
+        this.receive = o.receive;
     } else if (config.protocol === "tls") {
-
+        o = new tls(config);
+        this.connect = o.connect;
+        this.disconnect = o.disconnect; 
+        this.serve = o.serve;
+        this.send = o.send; 
+        this.receive = o.receive;
     } else if (config.protocol === "ws") {
-
+        o = new ws(config);
+        this.connect = o.connect;
+        this.disconnect = o.disconnect; 
+        this.serve = o.serve;
+        this.send = o.send; 
+        this.receive = o.receive;
     } else if (config.protocol === "socks") {
-
+        o = new socks(config);
+        this.connect = o.connect;
+        this.disconnect = o.disconnect; 
+        this.serve = o.serve;
+        this.send = o.send; 
+        this.receive = o.receive;
     } else if (config.protocol === "SOCKSv4") {
-
+        o = new sockv4(config);
+        this.connect = o.connect;
+        this.disconnect = o.disconnect; 
+        this.serve = o.serve;
+        this.send = o.send; 
+        this.receive = o.receive;
     } else if (config.protocol === "SOCKSv4a") {
-
+        o = new sockv4a(config);
+        this.connect = o.connect;
+        this.disconnect = o.disconnect; 
+        this.serve = o.serve;
+        this.send = o.send; 
+        this.receive = o.receive;
     } else if (config.protocol === "SOCKSv5") {
-
+        o = new sockv5(config);
+        this.connect = o.connect;
+        this.disconnect = o.disconnect; 
+        this.serve = o.serve;
+        this.send = o.send; 
+        this.receive = o.receive;
     } else if (config.protocol === "socket") {
-
+        o = new socket(config);
+        this.connect = o.connect;
+        this.disconnect = o.disconnect; 
+        this.serve = o.serve;
+        this.send = o.send; 
+        this.receive = o.receive;
     } else if (config.protocol === "ftp") {
-
+        o = new ftp(config);
+        this.connect = o.connect;
+        this.disconnect = o.disconnect; 
+        this.serve = o.serve;
+        this.send = o.send; 
+        this.receive = o.receive;
     } else if (config.protocol === "ftps") {
-
+        o = new ftps(config);
+        this.connect = o.connect;
+        this.disconnect = o.disconnect; 
+        this.serve = o.serve;
+        this.send = o.send; 
+        this.receive = o.receive;
     }
 
     return {
